7b157d8b7c17a09e6afadfb0047eeca681490806
Keven Souza <kevenssouza1@gmail.com>
2023-04-14 20:41:30 -0300

    basic layout style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950">
+          sidebar
+        </aside>
 
-    </main>
+        <main className="flex-1">
+          main
+        </main>
+      </div>
+
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
   )
 }
