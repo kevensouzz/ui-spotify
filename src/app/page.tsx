6e672e23b273ece70dff54703875fed370252963
Keven Souza <kevenssouza1@gmail.com>
2023-04-16 11:09:17 -0300

    add styles and components to Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import { Home as HomeIcon, Search, Library, BookmarkPlus, Heart, ChevronLeft, ChevronRight, PlayIcon } from "lucide-react"
+import { Laptop2, LayoutList, Maximize2, Mic2 } from "lucide-react"
+import { Volume2 } from "lucide-react"
+import { Home as HomeIcon, Search, Library, BookmarkPlus, Heart, ChevronLeft, ChevronRight, PlayIcon, Shuffle, SkipBack, SkipForward, Repeat } from "lucide-react"
 import Image from "next/image"
 
 export default function Home() {
@@ -106,7 +108,7 @@ export default function Home() {
 
           <h2 className="font-semibold text-2xl mt-8">Made for Keven Souza</h2>
 
-          <div className="grid grid-cols-8 gap-4 mt-4">
+          <div className="grid grid-cols-7 gap-4 mt-4">
 
             <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-1 hover:bg-white/10 ">
               <Image className="w-full" src={"/nevermind.jpg"} alt="capa do album Nevermind de Nirvana" width={104} height={104} />
@@ -141,8 +143,48 @@ export default function Home() {
         </main>
       </div>
 
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-5 py-1 flex items-center justify-between">
+
+        <div className="flex items-center gap-2">
+          <Image src={"/nevermind.jpg"} alt="capa do album Nevermind de Nirvana" width={36} height={36} />
+          <div className="flex flex-col">
+            <strong className="font-normal">Smells Like Teen Spirit</strong>
+            <span className="text-xs text-zinc-400">Nirvana</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-2">
+            <Shuffle size={18} className="text-zinc-200" />
+            <SkipBack size={18} className="text-zinc-200" />
+            <button className="w-6 h-6 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <PlayIcon />
+            </button>
+            <SkipForward size={18} className="text-zinc-200" />
+            <Repeat size={18} className="text-zinc-200" />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:44</span>
+            <div className="rounded-full w-80 bg-zinc-600">
+              <div className="bg-zinc-200 w-12 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">5:01</span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Mic2 size={18} />
+          <LayoutList size={18} />
+          <Laptop2 size={18} />
+          <div className="flex items-center gap-2">
+            <Volume2 size={18} />
+            <div className="rounded-full w-20 bg-zinc-600">
+              <div className="bg-zinc-200 w-20 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2 size={18} />
+        </div>
       </footer>
     </div>
   )
