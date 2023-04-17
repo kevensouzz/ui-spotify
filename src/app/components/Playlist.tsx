7b4183c3c41c418631381f2c8241a704fa367f8f
Keven Souza <kevenssouza1@gmail.com>
2023-04-17 20:20:10 -0300

    Update Playlist.tsx

--- src/app/components/Playlist.tsx
+++ src/app/components/Playlist.tsx
@@ -4,8 +4,8 @@ export default function Playlist() {
   return (
     <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-1 hover:bg-white/10 ">
       <Image className="w-full" src={"/nevermind.jpg"} alt="capa do album Nevermind de Nirvana" width={104} height={104} />
-      <strong className="font-semibold pt-1">TÍTULO</strong>
-      <span className="text-sm text-zinc-400">artístas</span>
+      <strong className="font-semibold pt-1">nome</strong>
+      <span className="text-sm text-zinc-400">usuário</span>
     </a>
   )
 }
