edc8807152be820a3da1cfe016a8206c1fd63930
Keven Souza <kevenssouza1@gmail.com>
2023-04-17 19:18:27 -0300

    Update ChevronButtons.tsx

--- src/app/components/ChevronButtons.tsx
+++ src/app/components/ChevronButtons.tsx
@@ -3,11 +3,11 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 export default function ChevronButtons() {
   return (
     <div className="flex items-center gap-3">
-      <button className="rounded-full bg-black/40 w-10 h-10 flex items-center justify-center pr-1 opacity-50">
+      <button className="rounded-full bg-black/40 w-10 h-10 flex items-center justify-center pr-1 opacity-50 cursor-default">
         <ChevronLeft />
       </button>
 
-      <button className="rounded-full bg-black/40 w-10 h-10 flex items-center justify-center pl-1 opacity-50">
+      <button className="rounded-full bg-black/40 w-10 h-10 flex items-center justify-center pl-1 opacity-50 cursor-default">
         <ChevronRight />
       </button>
     </div>
