15d9737426023c39ee4f959e8683441453809e05
Keven Souza <kevenssouza1@gmail.com>
2023-04-21 21:08:52 -0300

    update Logo.tsx

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Logo() {
   return (
       <a href="" className="flex gap-1">
-        <Image src={"/favicon-32x32.png"} alt={"Spotify's logo"} width={32} height={32} />
+        <Image src={"/logo/spotify-32x32.png"} alt={"Spotify's logo"} width={32} height={32} />
         <h1 className="font-semibold text-2xl">SPOTIFY</h1>
       </a>
   )
