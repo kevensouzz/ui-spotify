750fffef6160d43bf4efc736e6b30760b0da4584
Keven Souza <kevenssouza1@gmail.com>
2023-04-23 10:56:04 -0300

    sidebar component fixed

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -4,40 +4,40 @@ import MyPLaylist from "./MyPlaylist";
 export default function Sidebar() {
   return (
     <aside className="w-60 p-6">
-      <nav className="space-y-4">
-        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-          <HomeIcon size={18} />
-          Home
-        </a>
-        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-          <Search size={18} />
-          Search
-        </a>
-        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-          <Library size={18} />
-          Your Library
-        </a>
-        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-          <Compass size={18} />
-          Explore
-        </a>
-      </nav>
-
-      <div className="flex flex-col gap-3 mt-8 text-sm font-semibold">
-        <a href="" className="flex gap-3 text-zinc-200">
-          <BookmarkPlus size={18} />
-          Create Playlist
-        </a>
-        <a href="" className="flex gap-3 text-zinc-200">
-          <Heart size={18} />
-          Liked Songs
-        </a>
-      </div>
-
-      <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
-        <MyPLaylist nome="PHONK ESTRALANDO" />
-        <MyPLaylist nome="trap leozin/doka/vino" />
-        <MyPLaylist nome="sla qporré essa" />
+      <nav className="fixed">
+        <nav className="space-y-4">
+          <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <HomeIcon size={18} />
+            Home
+          </a>
+          <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <Search size={18} />
+            Search
+          </a>
+          <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <Library size={18} />
+            Your Library
+          </a>
+          <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <Compass size={18} />
+            Explore
+          </a>
+        </nav>
+        <div className="flex flex-col gap-3 mt-8 text-sm font-semibold">
+          <a href="" className="flex gap-3 text-zinc-200">
+            <BookmarkPlus size={18} />
+            Create Playlist
+          </a>
+          <a href="" className="flex gap-3 text-zinc-200">
+            <Heart size={18} />
+            Liked Songs
+          </a>
+        </div>
+        <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+          <MyPLaylist nome="PHONK ESTRALANDO" />
+          <MyPLaylist nome="trap leozin/doka/vino" />
+          <MyPLaylist nome="sla qporré essa" />
+        </nav>
       </nav>
 
     </aside>
