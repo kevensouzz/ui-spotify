a3627ba1f03806a7c9dcc2346978de8f0b8042e5
Keven Souza <kevenssouza1@gmail.com>
2023-05-01 19:36:16 -0300

    sidebar and footer to max-md

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,13 +10,16 @@ export default function Footer() {
       flex items-center justify-between
       fixed bottom-0 left-0 right-0
       max-lg:px-2
+      max-sm:p-1
     `}>
 
       <div className={`
         flex items-center gap-2
+        max-sm:gap-1
       `}>
 
-        <Image src={"/nevermind.jpg"} alt="capa do album Nevermind de Nirvana" width={36} height={36} />
+        <Image src={"/nevermind.jpg"} alt="capa do album Nevermind de Nirvana" width={36} height={36}
+        className={`max-md:w-7 h-7`} />
 
         <div className={`
           flex flex-col
@@ -25,9 +28,11 @@ export default function Footer() {
           <strong className={`
             font-normal
             max-lg:text-sm
+            max-sm:text-xs
           `}>Smells Like Teen Spirit</strong>
           <span className={`
             text-xs text-zinc-400
+            max-sm:text-zinc-500
           `}>Nirvana</span>
 
         </div>
@@ -39,33 +44,42 @@ export default function Footer() {
 
         <div className={`
           flex items-center gap-2
+          max-sm:gap-1
         `}>
 
           <Shuffle size={18} className={`
             text-zinc-200
+            max-sm: w-4 h-4
           `} />
           <SkipBack size={18} className={`
             text-zinc-200
+            max-sm: w-4 h-4
           `} />
           <button className={`
             w-6 h-6
+            max-sm:w-5 max-sm:h-5
             flex items-center justify-center
             rounded-full
             bg-white text-black
           `}>
-            <Play size={18} weight="fill" />
+            <Play size={18} weight="fill" className={`
+              max-sm: w-4 h-4
+            `} />
           </button>
           <SkipForward size={18} className={`
             text-zinc-200
+            max-sm: w-4 h-4
           `} />
           <Repeat size={18} className={`
             text-zinc-200
+            max-sm: w-4 h-4
           `} />
 
         </div>
 
         <div className={`
-          flex items-center gap-2 max-lg:hidden
+          flex items-center gap-2
+          max-lg:hidden
         `}>
 
           <span className={`
@@ -87,26 +101,40 @@ export default function Footer() {
 
       <div className={`
         flex items-center gap-3
+        max-sm:gap-1
       `}>
 
-        <Mic2 size={18} />
-        <LayoutList size={18} />
-        <Laptop2 size={18} />
+        <Mic2 size={18} className={`
+          max-sm: w-4 h-4
+        `}/>
+        <LayoutList size={18} className={`
+          max-sm: w-4 h-4
+        `}/>
+        <Laptop2 size={18} className={`
+          max-sm: w-4 h-4
+        `}/>
         <div className={`
           flex items-center gap-2
+          max-sm:gap-1
         `}>
 
-          <Volume2 size={18} />
+          <Volume2 size={18} className={`
+            max-sm: w-4 h-4
+          `}/>
           <div className={`
             rounded-full w-20 bg-zinc-600
+            max-sm:w-12
           `}>
             <div className={`
               bg-zinc-200 w-20 h-1 rounded-full
+              max-sm:w-12
             `}></div>
           </div>
         </div>
 
-        <Maximize2 size={18} />
+        <Maximize2 size={18} className={`
+          max-sm: w-4 h-4
+        `}/>
       </div>
     </footer>
   )
