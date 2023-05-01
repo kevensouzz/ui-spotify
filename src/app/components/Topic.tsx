0543e859fe0d96f13c84a5d99f5b36019a1ab542
Keven Souza <kevenssouza1@gmail.com>
2023-05-01 11:14:17 -0300

    Update Topic.tsx

--- src/app/components/Topic.tsx
+++ src/app/components/Topic.tsx
@@ -5,8 +5,12 @@ export default function Topic(prop: {text: string;}) {
       <div className={`
         flex items-center justify-between
         mt-8 mb-3
+        max-lg:mb-2 max-lg:mt-6
       `}>
-        <h1 className={`font-semibold text-3xl`}>{prop.text}</h1>
+        <h1 className={`
+          font-semibold text-3xl
+          max-lg:text-2xl
+        `}>{prop.text}</h1>
         <ChevronButtons />
       </div>
   )
